ca64b4de1b66128a4d57e763ceb483cfe712a2be
vid0326 <vidhutraushan9@gmail.com>
2025-11-05 09:12:37 +0530

    adde  url

--- youtube/backend/socket.js
+++ youtube/backend/socket.js
@@ -6,7 +6,7 @@ const server = http.createServer(app)
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:5173",
+        origin:"https://mini-tube-1lgv.vercel.app/",
         methods:["GET" , "POST" , "PUT" , "DELETE"]
     }
 })

c29ef3d38640334e683ffe8ff565bc5dba43fc4a
vid0326 <vidhutraushan9@gmail.com>
2025-11-05 06:48:59 +0530

    aadede origin

--- youtube/backend/index.js
+++ youtube/backend/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors({
-   origin:"http://localhost:5173",
+   origin:"https://mini-tube-1lgv.vercel.app/",
    credentials:true
 }))
 
